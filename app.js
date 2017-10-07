61f6b0fe56f53d35e1525ffc950c03eae46898d7
Edin <edinjusupovic@hotmail.com>
2017-10-07 03:17:26 +0000

    some changes, prepared rough layout

--- app.js
+++ app.js
@@ -1,1 +1,102 @@
-//test
+const { exec } = require('child_process');
+const fs = require('fs');
+
+
+// <== CPP FILES TO OBJECT ==> 
+async function cppToObject () {
+    return new Promise((resolve, reject) => {
+        exec('g++ -o build/example.o -c build/example.cpp', (err, stdout, stderr) => {
+        if (err) {
+            reject(err);
+  }
+  resolve('OBJECT BUILDING: [OK]'); 
+        });    
+    })
+}; 
+
+
+// <== OBJECT FILES TO EXECUTABLE ==> 
+async function objectToExecutable () {
+    return new Promise((resolve, reject) => {
+       exec('g++ -o build/foo build/example.o', (err, stdout, stderr) => {
+        if (err) {
+            reject('[DEUX ERROR]: ERROR WITH OBJECT TO EXE MODULE.');
+  }
+  resolve('EXECUTABLE BUILDING: [OK]');
+       });
+    });   
+}; 
+
+
+// <== DELETE OBJECT FILES FROM SYSTEM ==> 
+async function deleteObjectFiles () {
+    return new Promise((resolve, reject) => {
+        fs.unlink('build/example.o', (err) => {
+  if (err) {
+      reject('[DEUX ERROR]: ERROR DELETING OBJECT FILES.'); 
+  }
+  resolve('CLEANUP: [OK]');
+        });
+    });  
+}; 
+
+
+// <== DELETE EXECUTABLE FILES FROM SYSTEM ==> 
+async function deleteExeFiles () {
+    return new Promise((resolve, reject) => {
+        fs.unlink('build/foo', (err) => {
+  if (err) {
+      reject('[DEUX ERROR]: ERROR DELETING EXECUTABLE FILES.'); 
+  }
+  resolve('CLEANUP: [OK]');
+        });
+    });  
+}; 
+
+
+// <== EXECUTE COMPILED EXECUTABLE ==> 
+async function finalBuildExecution () {
+    return new Promise((resolve, reject) => {
+        exec('./build/foo', (err, stdout, stderr) => {
+        if (err) {
+            reject('[DEUX ERROR]: ERROR WITH FINAL BUILD EXECUTION.');
+  }
+  resolve(stdout); 
+        });     
+    })
+}; 
+
+
+(async () => {
+   try {
+//process.argv.forEach(function (val, index, array) {
+//  console.log(index + ': ' + val);
+//});
+       
+       let cppStatus = await cppToObject(); 
+        console.log(cppStatus);
+       
+       
+       let objectToExeStatus = await objectToExecutable();
+        console.log(objectToExeStatus);
+
+       console.log('<=== COMPILED C++ EXECUTABLE START ===>');
+       
+       let finalData = await finalBuildExecution();
+        console.log(finalData);
+       
+       let cleanObjectStatus = await deleteObjectFiles();
+        
+       
+       let cleanExeStatus = await deleteExeFiles();
+       
+       
+       if (cleanObjectStatus !== 'CLEANUP: [OK]' && cleanExeStatus !== 'CLEANUP: [OK]'){
+           throw 'COULD NOT DELETE GARBAGE FILES.';
+       };
+       
+       console.log('<=== COMPILED C++ EXECUTABLE END ===>');
+   } catch (error){
+       console.error(error);
+   }
+})(); 
--- readme.md
+++ readme.md
@@ -3,22 +3,49 @@
 
 Developed with bare simplicity in mind, Deux provides a build server system for C++ that monitors a target folder, automatically compiling when it detects any changes and executes compiled programs to provide you real-time feedback through the command line to assist in rapid development.
 
-Deux was developed as a custom solution for my personal needs, not as a public tool; the codebase however, may be of use to others.
+Real-time feedback is only provided through the command line in which Deux was executed from. It's worth noting that deux was developed as a custom solution for my personal needs, not as a public tool; the codebase however, may be of use to others.
 
 ## Requirements
 
 * CentOS 7 
 * NodeJS 8.6.0
 * G++ 
+* Nodemon (NPM)
 
 ## Getting started
 
+Install all dependencies:  
 
-Coming soon.
+```
+npm install -y 
+```
 
-## Usage example
+Execute Deux with a c++ file: 
 
-Coming soon.
+```
+nodemon deux.js -s example.cpp 
+```
+
+Deux output: 
+
+```
+[root@local deux]# nodemon --watch ./build/* app.js
+[nodemon] 1.11.0
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching: ./build/example.cpp
+[nodemon] starting `node app.js`
+OBJECT BUILDING: [OK]
+EXECUTABLE BUILDING: [OK]
+<=== COMPILED C++ EXECUTABLE START ===>
+Hello World!
+
+<=== COMPILED C++ EXECUTABLE END ===>
+[nodemon] clean exit - waiting for changes before restart
+```
+
+## Arguments
+
+All arguments after `-s` are considered to be your project .cpp files. 
 
 ## Features
 
